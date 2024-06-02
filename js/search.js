c8af29cb06e20b10937d9bc69e2eeafc50d5913c
Koomball <135945730+Koomball@users.noreply.github.com>
2024-06-02 21:47:57 +1000

    update js/search.js

--- js/search.js
+++ js/search.js
@@ -1,12 +1,10 @@
 function recentlyUploadedSearch() {
-  // Declare variables
   var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myMenu");
   div = ul.getElementsByTagName("div");
   
-  // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < div.length; i++) {
     a = div[i].getElementsByTagName("p")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
