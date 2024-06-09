cb2b7593e1982b7cb1d6b9e64054c24da70d0f06
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-09 20:19:45 +0530

    Update bdshighlighter.js

--- src/modules/bs-highlighter/bdshighlighter.js
+++ src/modules/bs-highlighter/bdshighlighter.js
@@ -443,6 +443,7 @@ arr = arr.replaceAll("$if", '<span style="color: #C27BD4;">' + "$if" + '</span>'
 arr = arr.replaceAll("$endif", '<span style="color: #C27BD4;">' + "$endif" + '</span>');
 arr = arr.replaceAll("$else", '<span style="color: #C27BD4;">' + "$else" + '</span>');
 arr = arr.replaceAll("$elseif", '<span style="color: #C27BD4;">' + "$elseif" + '</span>');
+arr = arr.replaceAll("if", '<span style="color: #C27BD4;">' + "if" + '</span>');
 
 // SYMBOLS
 arr = arr.replaceAll("[", '<span style="color: #779766;">' + "[" + '</span>');
