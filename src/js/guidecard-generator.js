e330f278ecf5c166e6004091020e5262c0c78dc6
Koomball <135945730+Koomball@users.noreply.github.com>
2024-06-11 02:47:47 +1000

    update src/js/guidecard-generator.js

--- src/js/guidecard-generator.js
+++ src/js/guidecard-generator.js
@@ -1,27 +1,28 @@
 const guides = [
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
-  ["BDScript", "Basic Ticket System"],
+  ["BDScript", "Basic Ticket System", "blue"],
+  ["BDScript", "Basic Ticket System", "blue"],
+  ["BDScript", "Basic Ticket System", "blue"],
+  ["BDScript", "Basic Ticket System", "blue"],
+  ["Forgescript", "Basic Ticket System", "purple"],
+  ["Forgescript", "Basic Ticket System", "purple"],
+  ["Forgescript", "Basic Ticket System", "purple"],
+  ["Forgescript", "Basic Ticket System", "purple"],
+  ["HTML", "Basic Ticket System", "red"],
+  ["HTML", "Basic Ticket System", "red"],
+  ["HTML", "Basic Ticket System", "red"],
+  ["HTML", "Basic Ticket System", "red"],
   ];
   
 a = document.getElementById("guidecards")
 b = [];
 
 for (i = 0; i <guides.length; i++) {
-
+  
   let x = guides[i][0];
   let y = guides[i][1];
+  let colour = guides[i][2];
   
-  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"><div class=\"top-container\"> <div class=\"Lol Lol--blue\">"+x+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+y+"</h4></div><div class=\"bottom\"><a href=\"/tag/bdfd/posts/variables-with-json/\"><div class='button -blue center'>View Code</div></a></div></div></div>"
+  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"><div class=\"top-container\"> <div class=\"Lol Lol--"+colour+"\">"+x+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+y+"</h4></div><div class=\"bottom\"><a href=\"/tag/bdfd/posts/variables-with-json/\"><div class='button -blue center'>View Code</div></a></div></div></div>"
 };
 
 a.innerHTML = b;
