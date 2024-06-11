d2c87bbc6c1845fc5710b155429525c6afaaab5b
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-12 00:50:46 +0530

    Update guidecard-generator.js
    forgot to remove \

--- src/js/guidecard-generator.js
+++ src/js/guidecard-generator.js
@@ -14,7 +14,7 @@ for (i = 0; i <guides.length; i++) {
   let colour = guides[i][2];
   let link = guides[i][3];
   
-  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"> <div class=\"top-container\"> <div class=\"Lol Lol--"+colour+"\">"+tag+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+title+"</h4></div><div class=\"bottom\"><a href=\"+link+\"><div class='button -blue center'>View Code</div></a></div></div></div>"
+  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"> <div class=\"top-container\"> <div class=\"Lol Lol--"+colour+"\">"+tag+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+title+"</h4></div><div class=\"bottom\"><a href="+link+"><div class='button -blue center'>View Code</div></a></div></div></div>"
 };
 
 a.innerHTML = b;
