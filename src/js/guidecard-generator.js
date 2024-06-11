20ce6fbe07ea71940c490c14abcec2ee9ed2d8b0
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-12 00:48:02 +0530

    Update guidecard-generator.js
    support for redirects

--- src/js/guidecard-generator.js
+++ src/js/guidecard-generator.js
@@ -1,28 +1,7 @@
 const guides = [
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["BDScript", "Basic Ticket System", "blue"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["Forgescript", "Basic Ticket System", "purple"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
-  ["HTML", "Basic Ticket System", "red"],
+  ["BDScript", "Variables with Json", "blue", "tag/bdfd/posts/variables-with-json"],
+  ["Forgescript", "Basic Ticket System", "purple", "#"],
+  ["HTML", "Basic Ticket System", "red", "#"],
   ];
   
 a = document.getElementById("guidecards")
@@ -30,11 +9,12 @@ b = [];
 
 for (i = 0; i <guides.length; i++) {
   
-  let x = guides[i][0];
-  let y = guides[i][1];
+  let tag = guides[i][0];
+  let title = guides[i][1];
   let colour = guides[i][2];
+  let link = guides[i][3];
   
-  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"><div class=\"top-container\"> <div class=\"Lol Lol--"+colour+"\">"+x+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+y+"</h4></div><div class=\"bottom\"><a href=\"/tag/bdfd/posts/variables-with-json/\"><div class='button -blue center'>View Code</div></a></div></div></div>"
+  b = b + "<div class=\"card3\" style=\"background-image: url(\'/logoImg/cardbg.png\');\"> <div class=\"top-container\"> <div class=\"Lol Lol--"+colour+"\">"+tag+"</div></div><div class=\"bottom-container\"><div class=\"middle\"><h4>"+title+"</h4></div><div class=\"bottom\"><a href=\"+link+\"><div class='button -blue center'>View Code</div></a></div></div></div>"
 };
 
 a.innerHTML = b;
